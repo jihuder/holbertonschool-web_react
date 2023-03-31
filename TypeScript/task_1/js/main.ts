d4b318c46c8d7fca1ec9c2b34e9e6ebfefb97d2f
jihuder <julioarenas85@gmail.com>
2023-03-31 14:36:34 -0500

    feat: crear una clase StudentClass con dos metodos
    metodos estar trabajando y mostrar nombre

--- TypeScript/task_1/js/main.ts
+++ TypeScript/task_1/js/main.ts
@@ -14,3 +14,28 @@ interface Directors extends Teacher {
 function printTeacher(firstName: string, lastName: string) {
     return `${firstName[0]}. ${lastName}`;
 }
+
+interface printTeacherFunction {
+    (firstName: string, lastName: string): string;
+}
+interface StudentInterface {
+    firstName: string;
+    lastName: string;
+}
+
+class StudentClass implements StudentInterface{
+    firstName: string;
+    lastName: string;
+    constructor(firstName: string, lastName: string) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    workOnHomework(): string {
+        return 'Currently working';
+    }
+
+    displayName(): string {
+        return this.firstName;
+    }
+}
