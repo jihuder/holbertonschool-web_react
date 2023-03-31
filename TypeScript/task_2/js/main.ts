a41a6af5c9c51b5fc4f1d3c63fc9ac8a3df5bd4c
jihuder <julioarenas85@gmail.com>
2023-03-31 15:04:05 -0500

    feat crear una variable con dos signifcados y una funcion que retorne que tipo de clase tiene

--- TypeScript/task_2/js/main.ts
+++ TypeScript/task_2/js/main.ts
@@ -56,3 +56,13 @@ function executeWork(employee: Teacher | Director): void {
         employee.workTeacherTasks();
     }
 }
+
+type Subjects = 'Math' | 'History';
+
+function teachClass(todayClass: Subjects): string {
+    if (todayClass === 'Math') {
+        return 'Teaching Math';
+    } else if (todayClass === 'History') {
+        return 'Teaching History';
+    }
+}
